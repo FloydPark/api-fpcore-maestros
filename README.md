ad2cf6099e7b6aa226b1234dcb9ca1a63ede4972
nancho313 <nancho313@hotmail.com>
2020-05-02 11:14:17 -0500

    initial content

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# testrepo
+# api-fpcore-maestros
+
+Microservicio que provee las funcionalidades de los recursos maestros, tales como 
+- Productos
+- Proveedores
+- Puntos de venta
+etc.
---
+++ src/test/java/com/floydpark/api/fpcore/maestros/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floydpark.api.fpcore.maestros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
